27ad4f193aa1b37d12f33a324209fdb35cae052a
Farshad Badiee <farshad.badiee@podro.com>
2022-12-19 17:45:32 +0330

    add domain and space crud to package

--- src/Zaya.php
+++ src/Zaya.php
@@ -20,13 +20,13 @@ class Zaya
 
     }
 
-    public function makeLink(string $link, string $domain = null,string $space=null)
+    public function makeLink(string $link, array $params = null)
     {
-        $body = [
+        $link = [
             'url' => $link,
-            'domain' => $domain,
-            'space' => $space,
         ];
+        $body = isset($params) ? array_merge($link, $params) : $link;
+
         $response = $this->client->post($this->baseUrl . 'links', [
             'form_params' => $body,
             'headers' => $this->headers
@@ -70,4 +70,89 @@ class Zaya
 
         return $response->getBody()->getContents();
     }
+
+    public function makeSpace(string $name, string $color)
+    {
+        $body = [
+            'name' => $name,
+            'color' => $color,
+        ];
+        $response = $this->client->post($this->baseUrl . 'spaces', [
+            'form_params' => $body,
+            'headers' => $this->headers
+        ]);
+        return json_decode($response->getBody()->getContents())->data;
+    }
+
+    public function listSpace()
+    {
+        $response = $this->client->get($this->baseUrl . 'spaces', [
+            'headers' => $this->headers
+        ]);
+        return json_decode($response->getBody()->getContents())->data;
+    }
+
+    public function detailSpace($id)
+    {
+        $response = $this->client->get($this->baseUrl . 'spaces/' . $id, [
+            'headers' => $this->headers
+        ]);
+        return collect(json_decode($response->getBody()->getContents())->data);
+    }
+
+    public function updateSpace($id, $name = null, $color = null)
+    {
+        $body = [
+            'name' => $name,
+            'color' => $color
+        ];
+        $response = $this->client->patch($this->baseUrl . 'spaces/' . $id, [
+            'form_params' => $body,
+            'headers' => $this->headers
+        ]);
+        return json_decode($response->getBody()->getContents())->data;
+    }
+
+    public function deleteSpace($id)
+    {
+        $response = $this->client->delete($this->baseUrl . 'spaces/' . $id, [
+            'headers' => $this->headers
+        ]);
+
+        return $response->getBody()->getContents();
+    }
+
+    public function listDomain()
+    {
+        $response = $this->client->get($this->baseUrl . 'domains', [
+            'headers' => $this->headers
+        ]);
+        return json_decode($response->getBody()->getContents())->data;
+    }
+
+    public function makeDomain(string $name, string $index = null, string $notFound = null)
+    {
+        $body = [
+            'name' => $name,
+            'index_page' => $index,
+            'not_found_page' => $notFound,
+        ];
+        try {
+            $response = $this->client->post($this->baseUrl . 'domains', [
+                'form_params' => $body,
+                'headers' => $this->headers
+            ]);
+        } catch(\Exception $e) {
+            return $e->getResponse()->getBody()->getContents();
+        }
+        return json_decode($response->getBody()->getContents())->data;
+    }
+    public function detailDomain($id)
+    {
+        $response = $this->client->get($this->baseUrl . 'domains/' . $id, [
+            'headers' => $this->headers
+        ]);
+        return collect(json_decode($response->getBody()->getContents())->data);
+    }
+
 }
